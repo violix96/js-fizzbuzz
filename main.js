fe249a68fc0b0c90d3afc5d27d014484d4abad29
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-30 19:01:05 +0200

    [UPD] Fizz condition satisfied

--- main.js
+++ main.js
@@ -3,5 +3,13 @@
 
 console.log("Ciao Mondo!")
 
-// Stampa i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {}
+// itero i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    // Se il numero è divisibile per 3 stampare "Fizz"
+    if (i % 3 === 0){
+        console.log("Fizz")
+    }else{
+        console.log(i);
+    }
+}
--- readme.md
+++ readme.md
@@ -9,3 +9,29 @@ Il programma stampa i numeri da 1 a 100 sulla console.
 Bonus 1: Crea un container nel DOM e aggiungi elementi con il numero o la stringa corretta.
 
 Bonus 2: Applica stili diversi agli elementi del DOM in base al contenuto (numero, "Fizz", "Buzz" o "FizzBuzz").
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
