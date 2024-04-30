35c80dd32643055f652844a7f573bbe963d67c21
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-30 19:10:34 +0200

    [UPD] FizzBuzz condition satisfied

--- main.js
+++ main.js
@@ -4,15 +4,18 @@
 console.log("Ciao Mondo!")
 
 // itero i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {
+for(let i = 1; i <= 100; i++) {
 
     // Se il numero è divisibile per 3 stampare "Fizz"
     if (i % 3 === 0){
         console.log("Fizz")
       // Se il numero è divisibile per 5 stampare "Buzz"   
-    }else if (i % 5 === 0){
+    }else if(i % 5 === 0){
         console.log("Buzz")
+        // "FizzBuzz" viene stampato al posto dei multipli sia di 3 che di 5.
+    }else if(i % 3 === 0 || i % 5 === 0) {
+        console.log("FizzBuzz")
     }else {
-        console.log(i)
+        console.log(i);
     }
 }
